7fa71a4fcd6fcc3c665719821b5caa949684f381
Guido van Rossum <guido@python.org>
2021-05-31 21:05:55 -0700

    Change architecture

--- pyco/pyco.py
+++ pyco/pyco.py
@@ -11,23 +11,20 @@ UNARY_NEGATIVE = dis.opmap["UNARY_NEGATIVE"]
 BUILD_TUPLE = dis.opmap["BUILD_TUPLE"]
 EXTENDED_ARG = dis.opmap["EXTENDED_ARG"]
 
-MAKE_INT = 170
-MAKE_LONG = 171
-MAKE_FLOAT = 172
-MAKE_STRING = 173
-# Etc.
-dis.opmap["MAKE_INT"] = MAKE_INT
-dis.opmap["MAKE_LONG"] = MAKE_LONG
-dis.opmap["MAKE_FLOAT"] = MAKE_FLOAT
-dis.opmap["MAKE_STRING"] = MAKE_STRING
-dis.opname[MAKE_INT] = "MAKE_INT"
-dis.opname[MAKE_LONG] = "MAKE_LONG"
-dis.opname[MAKE_FLOAT] = "MAKE_FLOAT"
-dis.opname[MAKE_STRING] = "MAKE_STRING"
+
+def def_op(name: str, op: int) -> int:
+    dis.opname[op] = name
+    dis.opmap[name] = op
+    return op
 
 
+MAKE_INT = def_op("MAKE_INT", 170)
+MAKE_LONG = def_op("MAKE_LONG", 171)
+MAKE_FLOAT = def_op("MAKE_FLOAT", 172)
+MAKE_STRING = def_op("MAKE_STRING", 173)
 
-def encode_varint(i):
+
+def encode_varint(i: int) -> bytes:
     """LEB128 encoding (https://en.wikipedia.org/wiki/LEB128)"""
     if i == 0:
         return b"\x00"
@@ -35,20 +32,20 @@ def encode_varint(i):
     b = bytearray()
     while i:
         bits = i & 127
-        i = i>>7
+        i = i >> 7
         if i:
             bits |= 128  # All but the final byte have the high bit set
         b.append(bits)
     return bytes(b)
 
 
-def encode_signed_varint(i):
+def encode_signed_varint(i: int) -> bytes:
     """Not LEB128; instead we put the sign bit in the lowest bit"""
     sign_bit = i < 0
-    return encode_varint(abs(i)<<1 | sign_bit)
+    return encode_varint(abs(i) << 1 | sign_bit)
 
 
-def encode_float(x):
+def encode_float(x: float) -> bytes:
     return struct.pack("<d", x)
 
 
@@ -77,60 +74,71 @@ SomeConstant = LongInt | Float | String
 
 
 class Builder:
+    # TODO: Intern duplicates
+
     def __init__(self):
         self.constants: list[SomeConstant] = []
 
-    def add(self, thing: SomeConstant):
+    def add(self, thing: SomeConstant) -> int:
         if thing.index < 0:
             thing.index = len(self.constants)
             self.constants.append(thing)
         assert thing.index >= 0
         return thing.index
 
+    def add_long(self, value: int) -> int:
+        return self.add(LongInt(value))
+
+    def add_float(self, value: float) -> int:
+        return self.add(Float(value))
+
+    def add_string(self, value: str) -> int:
+        return self.add(String(value))
+
+    def add_constant(self, value: object) -> int:
+        # TODO: API to add a generalized "Constant"
+        # (for which we generate code)
+        pass
+        
+
 
 class CodeGenerator:
-    # TODO: Intern constants
+    """Generate code for a constant."""
 
-    def __init__(self):
-        self.instructions: list[tuple[int, int | SomeConstant]] = []
-    
-    def emit(self, opcode: int, oparg: int | SomeConstant = 0):
+    def __init__(self, builder: Builder):
+        self.builder = builder
+        self.instructions: list[tuple[int, int]] = []
+
+    def emit(self, opcode: int, oparg: int = 0):
         self.instructions.append((opcode, oparg))
 
-    def generate(self, value):
+    def generate(self, value: object):
         match value:
-            case int():
-                if 0 <= value < 1<<16:
-                    self.emit(MAKE_INT, value)
-                elif -256 <= value < 0:
-                    self.emit(MAKE_INT, -value)
-                    self.emit(UNARY_NEGATIVE)
-                else:
-                    self.emit(MAKE_LONG, LongInt(value))
-            case float():
-                self.emit(MAKE_FLOAT, Float(value))
-            case str():
-                self.emit(MAKE_STRING, String(value))
-            case tuple():
+            case int(i) if 0 <= i < 1<<16:
+                self.emit(MAKE_INT, i)
+            case int(i) if -256 <= i < 0:
+                self.emit(MAKE_INT, -i)
+                self.emit(UNARY_NEGATIVE)
+            case int(i):
+                self.emit(MAKE_LONG, self.builder.add_long(i))
+            case float(x):
+                self.emit(MAKE_FLOAT, self.builder.add_float(x))
+            # TODO: complex, bool, None
+            case str(s):
+                self.emit(MAKE_STRING, self.builder.add_string(s))
+            # TODO: bytes
+            case tuple(t):
                 # TODO: Avoid needing a really big stack for large tuples
-                for item in value:
+                for item in t:
                     self.generate(item)
-                self.emit(BUILD_TUPLE, len(value))
+                self.emit(BUILD_TUPLE, len(t))
+            case _:
+                raise TypeError(
+                        f"Cannot generate code for "
+                        f"{type(value).__name__} -- {value!r}")
+                assert False, repr(value)
 
-    def fixup(self, builder):
-        # Replace non-int opargs with appropriate index values
-        for i, (opcode, oparg) in enumerate(self.instructions):
-            index = getattr(oparg, "index", oparg)
-            assert isinstance(index, int)
-            if index < 0:
-                index = builder.add(oparg)
-                assert index >= 0
-                oparg = index
-            self.instructions[i] = opcode, oparg
-            oparg = index
-
-    def get_bytes(self, builder):
-        self.fixup(builder)
+    def get_bytes(self):
         data = bytearray()
         for opcode, oparg in self.instructions:
             assert isinstance(oparg, int)
@@ -149,10 +157,10 @@ class CodeGenerator:
 
 
 def main():
-    cg = CodeGenerator()
     builder = Builder()
-    cg.generate((1000, -1, "Hi"))
-    dis.dis(cg.get_bytes(builder))
+    cg = CodeGenerator(builder)
+    cg.generate((0, 1000, -1, "Hi"))
+    dis.dis(cg.get_bytes())
 
 
 if __name__ == "__main__":
