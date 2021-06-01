e82b5276ff34f07cb1d6108dfaf90c97a09f4ce7
Guido van Rossum <guido@python.org>
2021-06-01 12:31:23 -0700

    Add support for code objects and other basic types

--- pyco/pyco.py
+++ pyco/pyco.py
@@ -8,7 +8,9 @@ from __future__ import annotations
 
 import dis
 import struct
-from typing import TypeVar
+import sys
+import types
+from typing import Iterator, TypeVar
 
 UNARY_NEGATIVE = dis.opmap["UNARY_NEGATIVE"]
 BUILD_TUPLE = dis.opmap["BUILD_TUPLE"]
@@ -24,7 +26,11 @@ def def_op(name: str, op: int) -> int:
 MAKE_INT = def_op("MAKE_INT", 170)
 MAKE_LONG = def_op("MAKE_LONG", 171)
 MAKE_FLOAT = def_op("MAKE_FLOAT", 172)
-MAKE_STRING = def_op("MAKE_STRING", 173)
+MAKE_COMPLEX = def_op("MAKE_COMPLEX", 173)
+MAKE_BYTES = def_op("MAKE_BYTES", 174)
+MAKE_STRING = def_op("MAKE_STRING", 175)
+MAKE_CODE_OBJECT = def_op("MAKE_CODE_OBJECT", 176)
+LOAD_COMMON_CONSTANT = def_op("LOAD_COMMON_CONSTANT", 177)  # None, False, True
 RETURN_CONSTANT = def_op("RETURN_CONSTANT", 179)
 LAZY_LOAD_CONSTANT = def_op("LAZY_LOAD_CONSTANT", 180)
 
@@ -70,6 +76,17 @@ class Float:
         return encode_float(self.value)
 
 
+class Bytes:
+    def __init__(self, value: bytes):
+        self.value = value
+
+    def get_bytes(self) -> bytes:
+        return encode_varint(len(self.value)) + self.value
+
+
+BlobConstant = LongInt | Float | Bytes
+
+
 class String:
     def __init__(self, value: str):
         self.value = value
@@ -78,9 +95,6 @@ class String:
         # Ecode number of bytes, not code points or characters
         b = self.value.encode("utf-8")
         return encode_varint(len(b)) + b
-
-
-BlobConstant = LongInt | Float
 
 
 class ComplexConstant:
@@ -95,6 +109,7 @@ class ComplexConstant:
         self.index = -1
 
     def set_index(self, index: int) -> None:
+        # Needed because RETURN_CONSTANT needs to know its own index
         self.index = index
 
     def emit(self, opcode: int, oparg: int, stackeffect: int):
@@ -104,6 +119,10 @@ class ComplexConstant:
 
     def generate(self, value: object):
         match value:
+            case None:
+                self.emit(LOAD_COMMON_CONSTANT, 0, 1)
+            case False | True as x:
+                self.emit(LOAD_COMMON_CONSTANT, int(x) + 1, 1)
             case int(i) if 0 <= i < 1<<16:
                 self.emit(MAKE_INT, i, 1)
             case int(i) if -256 <= i < 0:
@@ -113,10 +132,14 @@ class ComplexConstant:
                 self.emit(MAKE_LONG, self.builder.add_long(i), 1)
             case float(x):
                 self.emit(MAKE_FLOAT, self.builder.add_float(x), 1)
-            # TODO: complex, bool, None
+            case complex(real=re, imag=im):
+                self.emit(MAKE_FLOAT, self.builder.add_float(re), 1)
+                self.emit(MAKE_FLOAT, self.builder.add_float(im), 1)
+                self.emit(MAKE_COMPLEX, 0, -1)
+            case bytes(b):
+                self.emit(MAKE_BYTES, self.builder.add_bytes(b),1)
             case str(s):
                 self.emit(MAKE_STRING, self.builder.add_string(s), 1)
-            # TODO: bytes
             case tuple(t):
                 # TODO: Avoid needing a really big stack for large tuples
                 old_stacksize = self.stacksize
@@ -128,6 +151,8 @@ class ComplexConstant:
                 self.emit(BUILD_TUPLE, len(t), 1 - len(t))
                 assert self.stacksize == old_stacksize + 1, \
                     (self.stacksize, old_stacksize)
+            case types.CodeType() as code:
+                self.emit(MAKE_CODE_OBJECT, self.builder.add_code(code), 1)
             case _:
                 raise TypeError(
                         f"Cannot generate code for "
@@ -155,7 +180,16 @@ class ComplexConstant:
         return prefix + bytes(data)
 
 
-AnyConstant = String | BlobConstant | ComplexConstant
+class CodeObject:
+    def __init__(self, code: types.CodeType, builder: Builder):
+        self.code = code
+        self.builder = builder
+
+    def get_bytes(self) -> bytes:
+        ...
+
+
+AnyConstant = String | BlobConstant | ComplexConstant | CodeObject
 
 
 T = TypeVar("T")
@@ -165,6 +199,7 @@ class Builder:
     # TODO: Intern duplicates
 
     def __init__(self):
+        self.codeobjs: list[CodeObject] = []
         self.strings: list[String] = []
         self.blobs: list[BlobConstant] = []
         self.constants: list[ComplexConstant] = []
@@ -173,6 +208,9 @@ class Builder:
         index = len(where)
         where.append(thing)
         return index
+
+    def add_bytes(self, value: bytes) -> int:
+        return self.add(self.blobs, Bytes(value))
 
     def add_string(self, value: str) -> int:
         return self.add(self.strings, String(value))
@@ -189,10 +227,26 @@ class Builder:
         cc.set_index(index)
         return index
 
+    def add_code(self, code: types.CodeType) -> int:
+        return self.add(self.codeobjs, CodeObject(code, self))
 
-def main():
-    builder = Builder()
-    builder.add_constant((0, 1000, -1, "Hello world", "你好", 3.14, 0.5))
+
+def all_code_objects(code: types.CodeType) -> Iterator[types.CodeType]:
+    yield code
+    for x in code.co_consts:
+        if isinstance(x, types.CodeType):
+            yield x
+
+
+def add_everything(builder: Builder, code: types.CodeType):
+    for c in all_code_objects(code):
+        builder.add_code(c)
+        for x in c.co_consts:
+            if not isinstance(x, types.CodeType):
+                builder.add_constant(x)
+
+
+def report(builder: Builder):
     for i, constant in enumerate(builder.constants):
         b = constant.get_bytes()
         print(f"Code for constant {i} (prefix {b[:8].hex(' ')})")
@@ -205,6 +259,22 @@ def main():
     for i, blob in enumerate(builder.blobs):
         b = blob.get_bytes()
         print(f"{i:4d}: {b.hex(' ')}")
+
+
+def main():
+    builder = Builder()
+    if not sys.argv[1:]:
+        builder.add_constant(
+            (0, 1000, -1, "Hello world", "你好", b"hello world", 3.14, 0.5j)
+        )
+    else:
+        filename = sys.argv[1]
+        with open(filename, "rb") as f:
+            code = compile(f.read(), filename, "exec")
+            add_everything(builder, code)
+    report(builder)
+
+    report(builder)
 
 
 if __name__ == "__main__":
